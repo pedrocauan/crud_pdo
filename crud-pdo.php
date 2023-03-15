60a3ae39be0a2e7e8bd1f4e5cc9c6d54e25fea3d
pedrocauan <mainyasuo92@gmail.com>
2023-03-15 12:20:32 -0300

    insert

--- crud-pdo.php
+++ crud-pdo.php
@@ -1,8 +1,7 @@
 <?php 
+// -------------CONEXÃO --------------
 try{
-    //conexão
     $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost;", "usuario", "");
-
 } 
 //erros do pdo
 catch(PDOException $e) {
@@ -12,6 +11,19 @@ catch(PDOException $e) {
 catch (Exception $e) {
     echo "Erro genérico: " . $e-> getMessage();
 }
+
+
+// ----------- INSERT ------------------
+//1 forma
+$res = $pdo-> prepare("INSERT INTO pessoa(nome, telefone, email)values(:n, :t, :e)");
+
+$res-> bindValue(":n", "Pedroca");
+$res-> bindValue(":t", "1134062088");
+$res-> bindValue(":e", "teste@gmail.com");
+$res-> execute();
+
+//2 forma
+$pdo-> query("INSERT INTO pessoa(nome, telefone, email)VALUES('carlos', '11553455', 'carlete@gmail.com')");
 
 
 ?>
