7f2406694ec20a2d7151c7bc05a97f5779f0e876
pedrocauan <mainyasuo92@gmail.com>
2023-03-15 12:55:52 -0300

    select e listagem

--- crud-pdo.php
+++ crud-pdo.php
@@ -1,7 +1,8 @@
 <?php 
 // -------------CONEXÃO --------------
 try{
-    $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost;", "usuario", "");
+    //$options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+    $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost;", "usuario", "", $options);
 } 
 //erros do pdo
 catch(PDOException $e) {
@@ -13,7 +14,7 @@ catch (Exception $e) {
 }
 
 
-// ----------- INSERT ------------------
+// ----------- INSERT  (CREATE)------------------
 //1 forma
 // $res = $pdo-> prepare("INSERT INTO pessoa(nome, telefone, email)values(:n, :t, :e)");
 
@@ -34,12 +35,30 @@ catch (Exception $e) {
 // $cmd = $pdo-> query("DELETE FROM pessoa WHERE id = '4' ");
 
 // -------------- UPDATE --------------
-$cmd = $pdo-> prepare("UPDATE pessoa SET email = :e WHERE id = :id");
-$cmd-> bindValue(":e", "Pedroca@gmail.com");
-$cmd -> bindValue(":id", "1");
-$cmd -> execute();
+// $cmd = $pdo-> prepare("UPDATE pessoa SET email = :e WHERE id = :id");
+// $cmd-> bindValue(":e", "Pedroca@gmail.com");
+// $cmd -> bindValue(":id", "1");
+// $cmd -> execute();
 
 // $cmd = $pdo-> query("UPDATE pessoa SET email = 'pedrin@gmail.com' WHERE id = 1");
+
+// ------------ SELECT  ---------------
+$cmd = $pdo-> prepare("SELECT * FROM pessoa WHERE id = :id");
+$cmd->bindValue(":id", "3");
+$cmd->execute();
+
+$result = $cmd-> fetch(PDO::FETCH_ASSOC);
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
+//listagem na tela
+foreach($result as $key => $value){
+    echo "Campo: $key <br>";
+    echo "Valor: $value <br>";
+    echo "<br>";    
+}
+
 
 
 ?>
