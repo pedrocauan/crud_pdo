c638a6e6a68848928e243252818c593b7877f6b0
pedrocauan <mainyasuo92@gmail.com>
2023-03-15 11:46:14 -0300

    criação da database

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE CRUDPDO;
+
+USE CRUDPDO;
+
+CREATE TABLE pessoa(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(40),
+    telefone VARCHAR(15),
+    email VARCHAR(50)
+);
